c2bafafb3ab0b50dbd169a527366fbd8940b9f87
Xianzhao Duan <duanxianzhao@gmail.com>
2022-02-10 23:11:51 -0500

    Update web

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import Header from './components/header/Header'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import Navigation from './components/nav/Navigation'
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
+        <HashRouter>
           <Header/>
           <Navigation/>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     )
   }
